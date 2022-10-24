8bac4605d2b725bc83242f60719b7618bc9eb9b7
daghan <daghan.altunsoy@gmail.com>
2022-10-24 20:11:14 +0300

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-slim-buster
+FROM python:3.8-alpine
 
 RUN mkdir /app
 
@@ -7,5 +7,7 @@ WORKDIR /app/
 ADD app /app/
  
 RUN pip install -r requirements.txt
+
+EXPOSE 5000
 
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  msg = os.getenv('APP_MESSAGE')
+  return render_template('index.html', message=msg)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+  app.run(host="0.0.0.0", debug=True)
